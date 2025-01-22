426a1b9f18bd72677a2fbef40576ffb2f57bc09f
IsmaCar <160272744+IsmaCar@users.noreply.github.com>
2025-01-22 21:08:09 +0100

    Practica Despliegue AWS

---
+++ 02-PracticaDespliegueAWS/backend/src/Controller/ICMController.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+class ICMController
+{
+#[Route('/api/ini', name: 'api_ini')]
+public function index(): Response
+{
+return new Response('Symfony de Nombre Completo Responde a la llamada de
+React');
+}
+}
