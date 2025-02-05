b8eb1f9c9553102622dae9365b1240c0ef43cea5
IsmaCar <160272744+IsmaCar@users.noreply.github.com>
2025-02-05 20:53:58 +0100

    Modificacion Despliegue

--- DespliegueCompletoICM/backend/src/Controller/ICM_Controller.php
+++ DespliegueCompletoICM/backend/src/Controller/ICM_Controller.php
@@ -2,33 +2,29 @@
 
 namespace App\Controller;
 
-use App\Entity\TablaICM;
-use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\DBAL\Connection;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ICM_Controller extends AbstractController
 {
-
-    private EntityManagerInterface $entityManager;
-    public function __construct(EntityManagerInterface $entityManager)
+    private Connection $connection;
+    public function __construct(Connection $connection)
     {
-        $this->entityManager = $entityManager;
+        $this->connection = $connection;
     }
     #[Route('/api/db', name: 'get_db')]
     public function index(): JsonResponse
     {
-        $repository = $this->entityManager->getRepository(TablaICM::class);
-        $dato = $repository->findOneBy([]);
-
+        // Consulta para recuperar el primer mensaje de la tabla "messages"
+        $sql = 'SELECT fraseICM FROM secretosICM LIMIT 1';
+        $result = $this->connection->fetchOne($sql);
         // Si no hay mensaje en la BD, devolver un mensaje de error
-        if (!$dato) {
-
-            return $this->json(['message' => 'No se encontraron registros en la base de datos.']);
-
+        if (!$result) {
+            return $this->json(['message' => 'No messages found in the database!']);
         } else {
-            $result = 'Backend Operativo, respuesta de la BD: ' . $dato->getFraseICM();
+            $result = 'Backend Operativo, respuesta de la BD: ' . $result;
             return $this->json(['message' => $result]);
         }
     }
