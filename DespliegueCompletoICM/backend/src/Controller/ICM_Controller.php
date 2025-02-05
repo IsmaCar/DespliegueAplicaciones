441be97b6cd823fd4aaa9e5d6e94c776baed527e
IsmaCar <160272744+IsmaCar@users.noreply.github.com>
2025-02-05 21:02:52 +0100

    Modificacion Despliegue

--- DespliegueCompletoICM/backend/src/Controller/ICM_Controller.php
+++ DespliegueCompletoICM/backend/src/Controller/ICM_Controller.php
@@ -14,7 +14,7 @@ class ICM_Controller extends AbstractController
     {
         $this->connection = $connection;
     }
-    #[Route('/api/db', name: 'get_db')]
+    #[Route('/api/ICM', name: 'get_db')]
     public function index(): JsonResponse
     {
         // Consulta para recuperar el primer mensaje de la tabla "messages"
