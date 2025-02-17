5cb4a668b28b10959354b03590729e4fe71e11b4
IsmaCar <160272744+IsmaCar@users.noreply.github.com>
2025-02-17 17:42:19 +0100

    Practica proxy inverso

---
+++ DespliegueCompletoProxyInversoICM/frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { useEffect, useState } from 'react';
+
+import './App.css'
+function App() {
+  const [message, setMessage] = useState('');
+  useEffect(() => {
+    fetch('/api/ICM')
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }, []);
+  return (
+    <div>
+      <h1>Frontend en React de Ismael Carballo Martin</h1>
+      <p>Esta aplicación se conecta al backend de Symfony pidiéndole una respuesta</p>
+      <p>respuesta del Backend: {message || 'Cargando datos...'}</p>
+    </div>
+  );
+}
+export default App;
